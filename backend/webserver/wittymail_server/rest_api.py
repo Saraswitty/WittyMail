5261bbb5365bc57a6d1df8a6d3593176aa880be2
Ajay Nair <ajaynair59@gmail.com>
2018-11-08 19:44:39 +0530

    Skeleton REST implementation
    * Skeleton REST framework implementation

--- backend/webserver/wittymail_server/rest_api.py
+++ backend/webserver/wittymail_server/rest_api.py
@@ -12,8 +12,91 @@ _logger = logger.get_logger(__name__)
 from flask import jsonify, request
 import util.version as version
 
+headers =                   \
+[                           \
+'S.No',                     \
+'Name of Child',            \
+'Class',                    \
+'Sponsor',                  \
+'Reference',                \
+'Sponsor Mail Id',          \
+'Reference Mail Id',        \
+'Email subject',            \
+'Email body',               \
+'Attachment',               \
+'Has_sent_mail',            \
+]                           
+
+data =                      \
+[                           \
+[                           \
+'1',                        \
+'Avani Kulkarni',           \
+'Nur',                      \
+'Amit Shah',                \
+'Sumit Kumar',              \
+'amit.shah@gmail.com',      \
+'sumit.kumar@gmail.com',    \
+'Report card',              \
+'Hello Amit,\nPlease see attached the report card\nThanks,\nSneh Foundation\n', \
+'avani.kulkarni.pdf',       \
+False,                      \
+],                          \
+[                           \
+'2',                        \
+'Bhavani Shah',              \
+'Senior',                   \
+'Amar Shinde',              \
+'Sunil krishna',            \
+'amar.shinde@gmail.com',    \
+'sunilkrishna@gmail.com',   \
+'Report card',              \
+'Hello Amar,\nPlease see attached the report card\nThanks,\nSneh Foundation\n', \
+'bhavani.shah.pdf',         \
+False,                      \
+]                           \
+]
+
+# TODO Add comments to each function
+
 @flask_app.route("/api/version", methods=['GET'])
 def get_version():
     return (jsonify({'version': version.__pretty_version__}),
             200,
             {'ContentType':'application/json'})
+
+@flask_app.route("/api/fodder", methods=['POST'])
+def post_fodder():
+  return "", 200
+
+@flask_app.route("/api/fodder/ingredients", methods=['GET'])
+def get_fodder_ingredients():
+    return (jsonify(headers),
+            200,
+            {'ContentType':'application/json'})
+
+@flask_app.route("/api/attachment", methods=['POST'])
+def post_attachment():
+    return "", 200
+
+@flask_app.route("/api/email", methods=['POST'])
+def post_email():
+    return "", 200
+
+@flask_app.route("/api/email/test", methods=['POST'])
+def post_email_test():
+    return "", 200
+
+@flask_app.route("/api/email/send", methods=['POST'])
+def post_email_send():
+    return "", 200
+
+@flask_app.route("/api/vomit", methods=['GET'])
+def get_vomit():
+    return (jsonify({'headers': headers, 'data': data}),
+            200,
+            {'ContentType':'application/json'})
+
+@flask_app.route("/api/email_server", methods=['POST'])
+def post_email_server():
+    return "", 201
