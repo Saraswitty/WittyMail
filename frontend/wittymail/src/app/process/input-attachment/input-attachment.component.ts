a5a3799b415bbca54385a5907252a0f76ebb72a9
Omkar Ekbote <omkar.bugfix@live.com>
2018-11-16 20:29:29 +0530

    Write attachment-sheet page

--- frontend/wittymail/src/app/process/input-attachment/input-attachment.component.ts
+++ frontend/wittymail/src/app/process/input-attachment/input-attachment.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FileUploader, FileSelectDirective } from 'ng2-file-upload/ng2-file-upload';
+
+// TODO: Move this to the REST API service
+const URL = 'http://localhost:3000/api/upload';
 
 @Component({
   selector: 'app-input-attachment',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputAttachmentComponent implements OnInit {
 
+  public uploader: FileUploader = new FileUploader({url: URL, itemAlias: 'attachments'});
+  showDirectoryContents: boolean = false;
+  numFiles: number = 23;
+
   constructor() { }
 
   ngOnInit() {
+    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+    this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+         console.log('ImageUpload:uploaded:', item, status, response);
+         this.showDirectoryContents = true;
+     };
+  }
+
+  onFilesSelected(event: any) {
+    let files = [].slice.call(event.target.files);
+    console.log("Selected files: " + files);
+    this.numFiles = files.length;
+    this.showDirectoryContents = true;
   }
 
 }
