9c18500e56bd28dd82ad9faa3a349651f04d3fb3
Omkar Ekbote <omkar.bugfix@live.com>
2018-11-16 20:04:23 +0530

    Create contents of the input-sheet page

--- frontend/wittymail/src/app/app.module.ts
+++ frontend/wittymail/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FileSelectDirective } from 'ng2-file-upload';
+
 import { AboutComponent } from './about/about.component';
 import { InputSheetComponent } from './process/input-sheet/input-sheet.component';
 import { InputAttachmentComponent } from './process/input-attachment/input-attachment.component';
 
 @NgModule({
   declarations: [
+    FileSelectDirective,
+    
     AppComponent,
     AboutComponent,
     InputSheetComponent,
--- frontend/wittymail/src/app/process/input-sheet/input-sheet.component.ts
+++ frontend/wittymail/src/app/process/input-sheet/input-sheet.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FileUploader, FileSelectDirective } from 'ng2-file-upload/ng2-file-upload';
+
+// TODO: Move this to the REST API service
+const URL = 'http://localhost:3000/api/upload';
 
 @Component({
   selector: 'app-input-sheet',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputSheetComponent implements OnInit {
 
+  public uploader: FileUploader = new FileUploader({url: URL, itemAlias: 'sheet'});
+  showColumnSelectionForm: boolean = false;
+  sheetHeaders: string[] = ['Column 1', 'Column 2', 'Column 3', 'Column 4']
   constructor() { }
 
   ngOnInit() {
+    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+    this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+         console.log('ImageUpload:uploaded:', item, status, response);
+         this.showColumnSelectionForm = true;
+     };
   }
 
 }
