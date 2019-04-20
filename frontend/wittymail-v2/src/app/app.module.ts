bc98ac1a5525218e8d6c278a8ec9679eaed1540d
Omkar Ekbote <omkar.bugfix@live.com>
2019-04-20 20:49:23 +0530

    Add top-level stepper

--- frontend/wittymail-v2/src/app/app.module.ts
+++ frontend/wittymail-v2/src/app/app.module.ts
@@ -2,20 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatStepperModule} from '@angular/material/stepper';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StepsSliderComponent } from './steps-slider/steps-slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StepsSliderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
 
-    MatToolbarModule
+    MatToolbarModule,
+    MatStepperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
