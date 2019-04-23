9bbce7775a30d9b8f05a9f2dfc7681f73d1ace2f
Omkar Ekbote <omkar.bugfix@live.com>
2019-04-23 07:30:24 +0530

    Create sleletons for other components

--- frontend/wittymail-v2/src/app/app.module.ts
+++ frontend/wittymail-v2/src/app/app.module.ts
@@ -17,12 +17,22 @@ import { AttachmentsComponent } from './attachments/attachments.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material';
+import { SponsorListComponent } from './sponsor-list/sponsor-list.component';
+import { EmailDesignComponent } from './email-design/email-design.component';
+import { SummaryComponent } from './summary/summary.component';
+import { AboutComponent } from './common/about/about.component';
+import { ErrorDialogComponent } from './common/error-dialog/error-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StepsSliderComponent,
-    AttachmentsComponent
+    AttachmentsComponent,
+    SponsorListComponent,
+    EmailDesignComponent,
+    SummaryComponent,
+    AboutComponent,
+    ErrorDialogComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/wittymail-v2/src/app/email-design/email-design.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-email-design',
+  templateUrl: './email-design.component.html',
+  styleUrls: ['./email-design.component.css']
+})
+export class EmailDesignComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
