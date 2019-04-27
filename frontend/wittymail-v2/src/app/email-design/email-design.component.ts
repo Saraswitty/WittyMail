7687dc4cf0e1c35be1e0a6f9ba266e22973bab97
Omkar Ekbote <omkar.bugfix@live.com>
2019-04-27 16:17:56 +0530

    Email subject template with click-to-inset columns

--- frontend/wittymail-v2/src/app/email-design/email-design.component.ts
+++ frontend/wittymail-v2/src/app/email-design/email-design.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { LoggerService } from '../util/logger.service';
+import { MatInput } from '@angular/material';
 
 @Component({
   selector: 'app-email-design',
@@ -7,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailDesignComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild("subjectInput")
+  subjectInput: MatInput;
+
+  headers = ['Sr No.', 'Name of Child', 'Class', 'Sponsor', 'Mail ID', 'Reference', 'Reference Mail ID']
+  
+  sheetContents = [
+    {
+      'Sr No.': "1",
+      'Name of Child': "Aradhya Karche",
+      'Class': "Nursery-kalewadi",
+      'Sponsor': "Kalubai pratishthan",
+      'Mail ID': "Sanjaysandhu8090@gmail.com",
+      'Reference': "Ravi",
+      'Reference Mail ID': "amboreravi@gmail.com"
+    },
+    {
+      'Sr No.': "2",
+      'Name of Child': "Aradhya Karche",
+      'Class': "Nursery-kalewadi",
+      'Sponsor': "Kalubai pratishthan",
+      'Mail ID': "Sanjaysandhu8090@gmail.com",
+      'Reference': "Ravi",
+      'Reference Mail ID': "amboreravi@gmail.com"
+    },
+    {
+      'Sr No.': "3",
+      'Name of Child': "Aradhya Karche",
+      'Class': "Nursery-kalewadi",
+      'Sponsor': "Kalubai pratishthan",
+      'Mail ID': "Sanjaysandhu8090@gmail.com",
+      'Reference': "Ravi",
+      'Reference Mail ID': "amboreravi@gmail.com"
+    },
+  ];
+
+  subjectTemplate: string = "";
+  subjectSample: string = "This is the actual subject";
+
+  constructor(private log: LoggerService) { }
 
   ngOnInit() {
+  }
+
+  onClickColumn(columnName) {
+    this.log.info("Selected column: %s", columnName);
+    this.subjectTemplate += " {" + columnName + "} ";
   }
 
 }
