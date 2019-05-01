01538a6d53ab60e80e3b2536b817590200abbbb4
Omkar Ekbote <omkar.bugfix@live.com>
2019-05-01 18:11:59 +0530

    Connect the sponsor-list page to the backend

--- frontend/wittymail-v2/src/app/sponsor-list/sponsor-list.component.ts
+++ frontend/wittymail-v2/src/app/sponsor-list/sponsor-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
-import { BackendService } from '../backend.service';
+import { BackendService, ColumnHeadersWithRowContent } from '../backend.service';
 import { LoggerService } from '../util/logger.service';
 import { ErrorDialogComponent } from '../common/error-dialog/error-dialog.component';
 import { MatStepper } from '@angular/material';
@@ -12,52 +12,26 @@ import { MatStepper } from '@angular/material';
 })
 export class SponsorListComponent implements OnInit {
 
+  @Input('mainStepper') mainStepper: MatStepper;
   @ViewChild('stepper') stepper: MatStepper;
+  @ViewChild('columnMappingStepper') columnMappingStepper: MatStepper;
   @ViewChild('errorDialog') errorDialog: ErrorDialogComponent;
   public uploader: FileUploader;
 
-  headers = ['Sr No.', 'Name of Child', 'Class', 'Sponsor', 'Mail ID', 'Reference', 'Reference Mail ID']
-  columnSelectedAs = {
-    'Sr No.': '',
-    'Name of Child': 'Attachment Name',
-    'Class': '',
-    'Sponsor': '',
-    'Mail ID': 'E-mail Address (To)',
-    'Reference': '',
-    'Reference Mail ID': 'E-mail Address (CC)'
-  }
-  sheetContents = [
-    {
-      'Sr No.': "1",
-      'Name of Child': "Aradhya Karche",
-      'Class': "Nursery-kalewadi",
-      'Sponsor': "Kalubai pratishthan",
-      'Mail ID': "Sanjaysandhu8090@gmail.com",
-      'Reference': "Ravi",
-      'Reference Mail ID': "amboreravi@gmail.com"
-    },
-    {
-      'Sr No.': "2",
-      'Name of Child': "Aradhya Karche",
-      'Class': "Nursery-kalewadi",
-      'Sponsor': "Kalubai pratishthan",
-      'Mail ID': "Sanjaysandhu8090@gmail.com",
-      'Reference': "Ravi",
-      'Reference Mail ID': "amboreravi@gmail.com"
-    },
-    {
-      'Sr No.': "3",
-      'Name of Child': "Aradhya Karche",
-      'Class': "Nursery-kalewadi",
-      'Sponsor': "Kalubai pratishthan",
-      'Mail ID': "Sanjaysandhu8090@gmail.com",
-      'Reference': "Ravi",
-      'Reference Mail ID': "amboreravi@gmail.com"
-    },
-  ];
+  /** List of strings, each being the heading text for a column */
+  headers = [];
+
+  /** Dict of columnHeaderName -> selectedMapping */
+  columnSelectedAs = {};
+
+  /** Data displayed in the table */
+  sheetContents = [];
 
   constructor(private backend: BackendService, private log: LoggerService) { }
 
+  /**
+   * Setup the file uploader component's event handlers
+   */
   ngOnInit() {
     this.uploader = new FileUploader({ url: this.backend.getFodderUploadUrl(), itemAlias: 'sponsor-sheet' });
     this.uploader.onAfterAddingFile = (file) => {
@@ -69,11 +43,86 @@ export class SponsorListComponent implements OnInit {
       this.log.info('Sponsor sheet uploaded: ', item.file.name, status);
       if (status === 200) {
         this.stepper.next();
-        //this.displayColumnMappingUi();
+        this.displayColumnMappingUi();
       } else {
         this.errorDialog.showError("Failed to upload the sheet to the backend");
       }
     };
   }
 
+  /**
+   * Populate the table that displays the column headers and a few sample rows
+   */
+  displayColumnMappingUi() {
+    this.backend.getColumnHeadersWithSampleRows().subscribe(
+      data => {
+        this.log.info("Received column headers with samples: ", data);
+        let r: ColumnHeadersWithRowContent = <ColumnHeadersWithRowContent> data;
+        this.headers = r.headers;
+        this.sheetContents = r.contents;
+    
+        this.log.info("Got %d headers and %d rows", this.headers.length, this.sheetContents.length);
+      },
+      error => {
+        this.errorDialog.showError("Failed to analyze the Excel sheet");
+      }
+    );
+  }
+
+  /**
+   * Handle the button click event on a column header by noting it as the
+   * selected mapping for the currently active mapping step
+   * 
+   * @param selectedColumnHeader Name of the column being clicked
+   */
+  onSelectColumn(selectedColumnHeader: string) {
+    let currentColumnMapping = this.columnMappingStepper.selected.label;
+    this.log.info("Selected column %s for %s", selectedColumnHeader, currentColumnMapping)
+
+    this.columnSelectedAs[selectedColumnHeader] = currentColumnMapping;
+    
+    
+    if (this.columnMappingStepper.selectedIndex == this.columnMappingStepper.steps.length - 1) {
+      this.log.info("Done selecting columns, moving on...");
+      this.saveColumnMappingAndContinue();
+    } else {
+      this.columnMappingStepper.next();
+    }
+  }
+
+  /**
+   * Save the selected column mapping to the backend and move to the next page
+   */
+  saveColumnMappingAndContinue() {
+    let to_column: string = "";
+    let cc_column: string = "";
+    let attachment_column: string = "";
+
+    /* The map-stepper API returns a QueryList which, unlike an array, is not
+     * directly accesible by index, so convert it to a normal array first
+     */
+    let mappingSteps = [];
+    this.columnMappingStepper.steps.forEach(step => {mappingSteps.push(step)});
+
+    /* The dict is the other way round (columnHeader -> selectedMapping),
+     * so we have to loop through it
+     */
+    for (let key in this.columnSelectedAs) {
+      if (this.columnSelectedAs[key] == mappingSteps[0].label) {
+        to_column = key;
+      }
+      if (this.columnSelectedAs[key] == mappingSteps[1].label) {
+        cc_column = key;
+      }
+      if (this.columnSelectedAs[key] == mappingSteps[2].label) {
+        attachment_column = key;
+      }
+    }
+    this.log.info("Selected columns:\nto: %s\ncc: %s\nattachment: %s",
+    to_column, cc_column, attachment_column);
+    this.backend.saveEmailToCCColumns(to_column, cc_column);
+    this.backend.saveAttachmentMetadata(attachment_column);
+
+    this.mainStepper.next();
+  }
 }
